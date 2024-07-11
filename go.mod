6d5ca97ce3b84f681b106b3b9ba1b67bf251992a
Jack(ZhangYongguang) <zhang_yongguang@c.tre-inc.com>
2024-07-11 09:43:03 +0800

    init project

--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.12
 require (
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/handlers v1.4.2
-	github.com/gorilla/mux v1.6.2
-	github.com/lib/pq v0.0.0-20180523175426-90697d60dd84
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
 )
--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 var (
-	dbUser = os.Getenv("DB_USER")
+	dbUser     = os.Getenv("DB_USER")
 	dbPassword = os.Getenv("DB_PASSWORD")
-	dbName = os.Getenv("DB_NAME")
-	dbHost = os.Getenv("DB_HOST")
-	dbPort = os.Getenv("DB_PORT")
-	dbSSLMode = os.Getenv("DB_SSLMODE")
+	dbName     = os.Getenv("DB_NAME")
+	dbHost     = os.Getenv("DB_HOST")
+	dbPort     = os.Getenv("DB_PORT")
+	dbSSLMode  = os.Getenv("DB_SSLMODE")
 )
 
 func main() {
 	a := App{}
 
+	fmt.Printf("DB_USER: %s\n", dbUser)
+	fmt.Printf("DB_PASSWORD: %s\n", dbPassword)
+	fmt.Printf("DB_NAME: %s\n", dbName)
+	fmt.Printf("DB_HOST: %s\n", dbHost)
+	fmt.Printf("DB_PORT: %s\n", dbPort)
+	fmt.Printf("DB_SSLMODE: %s\n", dbSSLMode)
 	// use db connection credentials stored in env vars
 	a.Initialize(dbUser, dbPassword, dbName, dbHost, dbPort, dbSSLMode)
 
